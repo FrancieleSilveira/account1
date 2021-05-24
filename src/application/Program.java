42a0b7ea1a813917048ae64bfaf69d19ba05249e
Franciele Silveira <franciele.silveira1603@gmail.com>
2021-05-23 23:49:25 -0300

    tornando a classe Account abstrata

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,19 +1,37 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
 import entities.Account;
+import entities.BusinessAccount;
 import entities.SavingsAccount;
 
 class Program {
 
 	public static void main(String[] args) {
 		
-		Account x = new Account(1020, "Alex", 1000.0);
-		Account y = new SavingsAccount(1023, "Maria", 1000.0, 0.01);
+		Locale.setDefault(Locale.US); //para utilizar o . como separador de decimal
+		List<Account> list = new ArrayList<>();
 		
-		x.withdraw(50.0);
-		y.withdraw(50.0);
+		list.add(new SavingsAccount(1000, "Bruno", 500.00, 0.01));
+		list.add(new BusinessAccount(1001, "Maria", 1000.00, 400.0));
+		list.add(new SavingsAccount(1002, "joão", 300.00, 0.01));
+		list.add(new BusinessAccount(1003, "Fernanda", 500.50, 500.0));
 		
-		System.out.println(x.getBalance());
-		System.out.println(y.getBalance());
+		double sum = 0.0;
+		for (Account acc : list) {
+			sum += acc.getBalance();
+		}
+		
+		System.out.printf("Total balance: %.2f%n", sum);
+		
+		for (Account acc : list) {
+			acc.deposit(10.0);
+		}
+		for (Account acc: list) {
+			System.out.printf("Updates balance for account %d: %.2f%n", acc.getNumber(), acc.getBalance());
+		}
 	}
 }
